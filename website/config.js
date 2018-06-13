666c8255d5aabddada3a689242decedbc7d08734
Ned <iamcalledned@gmail.com>
2018-06-13 13:14:31 -0400

    Update config.js

--- website/config.js
+++ website/config.js
@@ -1,6 +1,6 @@
-var api = "http://poolhost.com:1117";
+var api = "http://18.221.222.198:8117";
 
-var poolHost = "poolhost.com";
+var poolHost = "18.221.222.198";
 
 var irc = "irc.freenode.net/#poolhost";
 
@@ -10,8 +10,9 @@ var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://explorer.havenprotocol.com/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://explorer.havenprotocol.com/tx/{id}";
+
 
 var themeCss = "themes/default-theme.css";
